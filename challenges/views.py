8bc30c04f013d4e885c6c579e98583f567a23958
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-03 16:04:05 +0100

    add challenges templates

--- challenges/views.py
+++ challenges/views.py
@@ -15,28 +15,18 @@ monthly_challenges = {
     "september": "Learn Django for at least 20 minutes every day!",
     "october": "Eat no meat for the entire month!",
     "november": "Walk for at least 20 minutes every day!",
-    "december": "Learn Django for at least 20 minutes every day!"
+    "december": None
 }
 
 # Create your views here.
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = """
-    <h1>Monthly Challenges</h1>
-    <ul>
-        """ + list_items + """
-    </ul>
-    """
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -56,7 +46,11 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        return render(request, "challenges/challenge.html",
+                      {
+                          "text": challenge_text,
+                          "month_name": month
+                        })
     except:
         return HttpResponseNotFound("This month is not supported!")
     
