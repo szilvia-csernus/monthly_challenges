af3a8bc315b5c4b56aa459f8683f58608cf0e726
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-03 13:19:47 +0100

    add challenges urls and views

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
+}
 
 # Create your views here.
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = """
+    <h1>Monthly Challenges</h1>
+    <ul>
+        """ + list_items + """
+    </ul>
+    """
+    return HttpResponse(response_data)
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month!")
+    
+    redirect_month = months[month - 1]
+    # we don't want to hardcode 'challenges/' here into the path becaure if
+    # that gets changed in monthly_challenges.urls.py these paths won't be 
+    # found. Instead we can use reverse to generate the path dynamically
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenges/january
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+    
